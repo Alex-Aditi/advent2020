2cff21f6be3b3fe07eb5e652d32b1b3db9d2ec61
Aditi Ohri <didiohri@gmail.com>
2021-01-04 19:57:16 -0600

    remove console.log

--- day1/day1part1.js
+++ day1/day1part1.js
@@ -12,6 +12,5 @@ let data = nums.split('\n').map(n => parseInt(n))
 
 data = data.sort((a, b) => a - b)
 
-console.log(data)
 // look through each potential pairing to see if it sums to 2020
 // if it does, return value of the product
