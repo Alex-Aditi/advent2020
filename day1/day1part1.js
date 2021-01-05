8280960e54b803e3819c5e7d9f0a7936d405534d
Aditi Ohri <didiohri@gmail.com>
2021-01-04 19:45:56 -0600

    initialize data into an array

--- day1/day1part1.js
+++ day1/day1part1.js
@@ -1,1 +1,15 @@
 const { read } = require("../utilities/import");
+
+
+// find product of two numbers that sum to 2020
+
+// access the list of numbers
+const nums = read('./day1data-alexandra')
+
+// arrange them somehow
+// initialize an array
+const data = nums.split('\n').map(n => parseInt(n))
+console.log(data.length)
+
+// look through each potential pairing to see if it sums to 2020
+// if it does, return value of the product
